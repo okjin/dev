5f1270994a4ca46d2993da3bb9baaa4c1f3c9e52
okjin <holydrama@gmail.com>
2015-04-09 23:23:56 +0900

    B, C 기지국일때 체크 완료

--- SwVerify/src/Solution1.java
+++ SwVerify/src/Solution1.java
@@ -24,7 +24,7 @@ class Solution1
 		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
 		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
 		 */
-		System.setIn(new FileInputStream("sample_input_11.txt"));
+		System.setIn(new FileInputStream("sample_input_1.txt"));
 
 		/*
 		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
@@ -49,30 +49,44 @@ class Solution1
 					map[i][j] = tmp.charAt(0);
 				}
 			}
-/*
-X X X X X X X X X
-X X X H X X X X X
-X X H A H X X H X
-X X H H X X X X X
-X X X X X X X X X
-X X A H H X X X X
-X X H X X H A H X
-X X A H X X H X X
-X X H X H X X X X
-*/
 			/////////////////////////////////////////////////////////////////////////////////////////////
 			/*
 			 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
 			 여러분의 정답은 AnswerN에 저장되는 것을 가정하였습니다.
 			 */
 			/////////////////////////////////////////////////////////////////////////////////////////////
+			AnswerN = 0;
 			for(int i = 0 ; i < N ; i++) {
 				for(int j = 0 ; j < N ; j++) {
 					if (map[i][j] == 'A') {
-						if(i-1 != -1 && map[i-1][j] == 'H') map[i-1][j] = 'X';
-						if(i+1 != N  && map[i+1][j] == 'H') map[i+1][j] = 'X';
-						if(j-1 != -1 && map[i][j-1] == 'H') map[i][j-1] = 'X';
-						if(j+1 != N  && map[i][j+1] == 'H') map[i][j+1] = 'X';
+						if(i-1 >= 0 && map[i-1][j] == 'H') map[i-1][j] = 'X';
+						if(i+1 < N && map[i+1][j] == 'H') map[i+1][j] = 'X';
+						if(j-1 >= 0 && map[i][j-1] == 'H') map[i][j-1] = 'X';
+						if(j+1 < N && map[i][j+1] == 'H') map[i][j+1] = 'X';
+					}
+					if (map[i][j] == 'B') {
+						if(i-1 >= 0 && map[i-1][j] == 'H') map[i-1][j] = 'X';
+						if(i-2 >= 0 && map[i-2][j] == 'H') map[i-2][j] = 'X';
+						if(i+1 < N && map[i+1][j] == 'H') map[i+1][j] = 'X';
+						if(i+2 < N && map[i+2][j] == 'H') map[i+2][j] = 'X';
+						if(j-1 >= 0 && map[i][j-1] == 'H') map[i][j-1] = 'X';
+						if(j-2 >= 0 && map[i][j-2] == 'H') map[i][j-2] = 'X';
+						if(j+1 < N && map[i][j+1] == 'H') map[i][j+1] = 'X';
+						if(j+2 < N && map[i][j+2] == 'H') map[i][j+2] = 'X';
+					}
+					if (map[i][j] == 'C') {
+						if(i-1 >= 0 && map[i-1][j] == 'H') map[i-1][j] = 'X';
+						if(i-2 >= 0 && map[i-2][j] == 'H') map[i-2][j] = 'X';
+						if(i-3 >= 0 && map[i-3][j] == 'H') map[i-3][j] = 'X';
+						if(i+1 < N && map[i+1][j] == 'H') map[i+1][j] = 'X';
+						if(i+2 < N && map[i+2][j] == 'H') map[i+2][j] = 'X';
+						if(i+3 < N && map[i+3][j] == 'H') map[i+3][j] = 'X';
+						if(j-1 >= 0 && map[i][j-1] == 'H') map[i][j-1] = 'X';
+						if(j-2 >= 0 && map[i][j-2] == 'H') map[i][j-2] = 'X';
+						if(j-3 >= 0 && map[i][j-3] == 'H') map[i][j-3] = 'X';
+						if(j+1 < N && map[i][j+1] == 'H') map[i][j+1] = 'X';
+						if(j+2 < N && map[i][j+2] == 'H') map[i][j+2] = 'X';
+						if(j+3 < N && map[i][j+3] == 'H') map[i][j+3] = 'X';
 					}
 				}
 			}
