a72fe92ccfa8e5bb21472ad63ba2e03836333650
okjin.choi <okjin.choi@samsung.com>
2015-04-15 18:42:27 +0900

    스도쿠 문제 완료

---
+++ SwVerify/src/P2178.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class P2178 {
+	
+	private static class Location{
+		public final int x;
+		public final int y;
+		public final int distance;
+		public Location(int x, int y, int distance){
+			this.x = x;
+			this.y = y;
+			this.distance = distance;
+		}
+	}
+
+	public static void main(String[] args) {
+		final Scanner sc = new Scanner(System.in);
+		final int n = sc.nextInt();
+		final int m = sc.nextInt();
+		sc.nextLine();
+
+		final int maze[][] = new int[n][m];
+		for(int i=0; i<n; i++){
+			final char[] line = sc.nextLine().toCharArray();
+			for(int j=0; j<m; j++){
+				maze[i][j] = Integer.parseInt(String.valueOf(line[j]));
+			}
+		}//end of getting inputs
+		
+		//BFS
+		final boolean[][] visited = new boolean[n][m];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				visited[i][j] = false;
+			}
+		}
+		
+		final int[][] distances = new int[n][m];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				distances[i][j] = 0;
+			}
+		}
+		
+		final Queue<Location> queue = new LinkedList<Location>();
+		queue.add(new Location(0, 0, 1));
+		visited[0][0] = true;
+
+		while(!queue.isEmpty()){
+			final Location location = queue.poll();
+			final int x = location.x;
+			final int y = location.y;
+			final int distance = location.distance;
+
+			distances[x][y] = distance;
+
+			if(x>0 && maze[x-1][y] == 1 && !visited[x-1][y]){
+				queue.add(new Location(x-1, y, distance+1));
+				visited[x-1][y] = true;
+			}
+			if(x<n-1 && maze[x+1][y] == 1 && !visited[x+1][y]){
+				queue.add(new Location(x+1, y, distance+1));
+				visited[x+1][y] = true;
+			}
+			if(y>0 && maze[x][y-1] == 1 && !visited[x][y-1]){
+				queue.add(new Location(x, y-1, distance+1));
+				visited[x][y-1] = true;
+			}
+			if(y<m-1 && maze[x][y+1] == 1 && !visited[x][y+1]){
+				queue.add(new Location(x, y+1, distance+1));
+				visited[x][y+1] = true;
+			}
+		}
+		System.out.println(distances[n-1][m-1]);
+		sc.close();
+	}
+}
--- SwVerify/src/Sudoku.java
+++ SwVerify/src/Sudoku.java
@@ -43,7 +43,7 @@ class Sudoku
 
 			N = sc.nextInt();	// 틀린 갯수
 			
-//			AnswerArr = new String[N][2];	// 틀린위치, 정답
+			AnswerArr = new String[N][2];	// 틀린위치, 정답
 
 			char[][] map = new char[C][C];
 			for(int i = 0 ; i < C ; i++) {
@@ -62,67 +62,111 @@ class Sudoku
 			// Input 출력
 			printInput(map);
 
-			// 틀린 위치 찾기
+			// 틀린 곳 찾기 tmpList 저장
 			findWrongPosition(map);
 			
 			for (int i=0; i < TmpList.size(); i++) {
-				System.out.println("AnsewerL : " + i + " " + TmpList.get(i).toString());
+				System.out.println("TmpList : " + i + " " + TmpList.get(i).toString());
+			}
+			
+			// 틀린 곳 정확하게 검증하고 맞는 답 찾기
+			for (int i=0; i < TmpList.size(); i++) {
+				int x, y;
+				String[] tmp = TmpList.get(i).split(",");
+				
+				// 앞 위치 검증
+				x = new Integer(tmp[0].charAt(0)+"");
+				y = new Integer(tmp[0].charAt(1)+"");
+				
+				if (findWrongVer(x, y, map) > -1) {
+					System.out.println("Got it !!!!" + x + "," + y);
+					AnswerArr[i][0] = tmp[0].charAt(0)+" "+ tmp[0].charAt(1)+"";
+					AnswerArr[i][1] = fixIt(x, y, map);
+				}
+				
+				// 뒷 위치 검증
+				x = new Integer(tmp[1].charAt(0)+"");
+				y = new Integer(tmp[1].charAt(1)+"");
+				
+				if (findWrongVer(x, y, map) > -1) {
+					System.out.println("Got it !!!!" + x + "," + y);
+					AnswerArr[i][0] = tmp[1].charAt(0)+" "+ tmp[1].charAt(1)+"";
+					AnswerArr[i][1] = fixIt(x, y, map);
+				}
 			}
 			
 			// 표준출력(화면)으로 답안을 출력합니다.
-//			for(int i = 0 ; i < N ; i++) {
-//				
-//				System.out.println("#"+test_case+" : "+AnswerArr[i][0]+" "+AnswerArr[i][1]);
-//			}
+			for(int i = 0 ; i < N ; i++) {
+				System.out.println("#"+i+" : "+AnswerArr[i][0]+" "+AnswerArr[i][1]);
+			}
 		}
+	}
+	
+	// 정답 찾기
+	private static String fixIt(int x, int y, char[][] a) {
+		
+		char[] tmp = {'x','x','x','x','x','x','x','x','x','x'};		// 10개 'x' 로 초기화(배열은 0부터니까)
+		
+		for (int i=0; i < C; i++) {
+			if (x != i) {
+				tmp[new Integer(a[i][y]+"")] = 'o';
+			}
+		}
+		System.out.println("fixIt : " + new String(tmp));
+		
+		for (int i=1; i < tmp.length; i++) {
+			if (tmp[i] == 'x') return Integer.toString(i);
+		}
+		
+		return null;
+		
 	}
 	
 	// 틀린 위치 찾기
 	private static void findWrongPosition(char[][] a) {
 		TmpList = new ArrayList<String>();
 		
+		// 가로 검색
 		for (int i=0; i < C; i++) {
 			if (N == WrongCnt) break;
 			
 			for (int j=0; j < C; j++) {
 				if (N == WrongCnt) break;
-				findWrongByChar(i, j, a);
+				findWrongHor(i, j, a);
 			}
 		}
+
 	}
 	
-	// 현재 위치에서 가로, 세로 검색해서 중복된 위치 저장
-	private static int findWrongByChar(int x, int y, char[][] a) {
+	// 가로 검색(x 축 고정, y만 이동하면서), 중복된 위치 저장
+	private static int findWrongHor(int x, int y, char[][] a) {
 		
 		String tmp = "";
 		
-		if (N == WrongCnt) return 1;
-		
-		// 가로 검색
-		for (int i=0; i < C; i++) {
-			// 자신 위치는 Skip, 자신 위치 값과 순차적으로 비교
-			if (i != y && a[x][y] == a[x][i]) {
-				tmp = Integer.toString(x) + Integer.toString(y) + Integer.toString(x) + Integer.toString(i);
-				TmpList.add(tmp);
-				WrongCnt ++;
-				break;
-			}
-		}
-		
-		// 세로 검색 
-		for (int i=0; i < C; i++) {
-			// 자신 위치는 Skip, 자신 위치 값과 순차적으로 비교
-			if (i != x && a[x][y] == a[i][y]) {
-				tmp = Integer.toString(x) + Integer.toString(y) + Integer.toString(i) + Integer.toString(y);
-				TmpList.add(tmp);
+		for (int i = y+1; i < C; i++) {
+			// 자신 위치는 Skip, 현재 위치 값과 순차적으로 비교
+			if (a[x][y] == a[x][i]) {
+				tmp = Integer.toString(x) + Integer.toString(y) + "," + Integer.toString(x) + Integer.toString(i);
+				TmpList.add(tmp);	// tmpList 에 저장할때는 앞,뒤 자표 어느값이 잘못된 것인지 모름 (틀린 위치 찾기를 통해서 확인)
 				WrongCnt ++;
 			}
 		}
-	
+		
 		return 0;
 	}
 	
-
+	// 세로 검색(y 축 고정, x만 이동하면서) 검증용(세로로 검색해서 중복되면 해당 위치가 잘못된 값)
+	private static int findWrongVer(int x, int y, char[][] a) {
+		
+		for (int i=0; i < C; i++) {
+			// 자신 위치는 Skip, 현재 위치 값과 순차적으로 비교
+			if (x != i && a[x][y] == a[i][y]) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
 
 
 	private static void printInput(char[][] in) {
