7d28c84ca379bf079c9ff897bbf8a94e72cf65e9
okjin.choi <okjin.choi@samsung.com>
2015-04-16 10:35:39 +0900

    DFS 완성

--- SwVerify/src/Solution_DFS.java
+++ SwVerify/src/Solution_DFS.java
@@ -17,6 +17,7 @@ class Solution_DFS
 {
 	static int N;
 	static Boolean[][] Visited;
+	static Queue<Node> queue;
 	
 	private static class Node {
 		private int x, y;
@@ -60,17 +61,13 @@ class Solution_DFS
 			}
 			
 
-			for(int i = 0 ; i < N ; i++) {
-				for(int j = 0 ; j < N ; j++) {
-					Visited[i][j] = false;
-				}
-			}
+			initVisited();
 			
-			Queue<Node> queue = new LinkedList<Node>();
+			queue = new LinkedList<Node>();
 			queue.add(new Node(0,0,1));
 			Visited[0][0] = true;
 			
-			DFS(0,0,map, queue);
+			DFS(0,0,map);
 			
 			System.out.println("---" + queue.size());
 			while(!queue.isEmpty()) {
@@ -83,46 +80,56 @@ class Solution_DFS
 		}
 	}
 	
+	private static void initVisited() {
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				Visited[i][j] = false;
+			}
+		}
+	}
 
-	private static void DFS(int x, int y, char[][] map, Queue<Node> queue) {
-//		Node node = queue.poll();
-//		int x = node.x;
-//		int y = node.y;
-//		int d = node.distance;
-		
+	private static void DFS(int x, int y, char[][] map) {
 //		System.out.println("222 : " + queue.size());
-		System.out.println("DFS : " + x + " " + y);
+//		System.out.println("DFS : " + x + " " + y);
 		
 		if (x == N-1 && y == N-1) {
 			System.out.println("Arrived!!!");
+			return;
 		}
 		else {
 			// µ¿
 			if (y < N-1 && map[x][y+1] == 'O' && !Visited[x][y+1]) {
 				queue.add(new Node(x, y+1));
 				Visited[x][y+1] = true;
-				DFS(x, y+1, map, queue);
+				DFS(x, y+1, map);
 			}
-			
 			// ³²
-			if (x < N-1 && map[x+1][y] == 'O' && !Visited[x+1][y]) {
+			else if (x < N-1 && map[x+1][y] == 'O' && !Visited[x+1][y]) {
 				queue.add(new Node(x+1, y));
 				Visited[x+1][y] = true;
-				DFS(x+1, y, map, queue);
+				DFS(x+1, y, map);
 			}
-			
 			// ¼­
-			if (y > 0 && map[x][y-1] == 'O' && !Visited[x][y-1]) {
+			else if (y > 0 && map[x][y-1] == 'O' && !Visited[x][y-1]) {
 				queue.add(new Node(x, y-1));
 				Visited[x][y-1] = true;
-				DFS(x, y-1, map, queue);
+				DFS(x, y-1, map);
 			}
-			
 			// ºÏ
-			if (x > 0 && map[x-1][y] == 'O' && !Visited[x-1][y]) {
+			else if (x > 0 && map[x-1][y] == 'O' && !Visited[x-1][y]) {
 				queue.add(new Node(x-1, y));
 				Visited[x-1][y] = true;
-				DFS(x-1, y, map, queue);
+				DFS(x-1, y, map);
+			}
+			else {
+				if (x == 0 && y == 0) return;
+				initVisited();
+				map[x][y] = 'X';
+				
+				queue = new LinkedList<Node>();
+				queue.add(new Node(0,0,1));
+				Visited[0][0] = true;
+				DFS(0,0,map);
 			}
 		}
 	}
